7488688014d368f653d6e5203b6145fd4f00edf1
boxkon <ghkdxo14@gmail.com>
2020-12-02 20:24:47 +0900

    핑거 삭제 및 lqr 튜닝

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -49,8 +49,8 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1855. + STD_CARGO_KG
       ret.wheelbase = 3.01
     elif candidate == CAR.GENESIS_G90:
-      ret.mass = 2200
-      ret.wheelbase = 3.15
+      ret.mass = 2120
+      ret.wheelbase = 3.16
     elif candidate == CAR.GENESIS_G90_L:
       ret.mass = 2290
       ret.wheelbase = 3.45
@@ -132,24 +132,24 @@ class CarInterface(CarInterfaceBase):
 
     ret.lateralTuning.init('lqr')
 
-    ret.lateralTuning.lqr.scale = 1900.
+    ret.lateralTuning.lqr.scale = 1680.
     ret.lateralTuning.lqr.ki = 0.01
-    ret.lateralTuning.lqr.dcGain = 0.0029
+    ret.lateralTuning.lqr.dcGain = 0.002858
 
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
     ret.lateralTuning.lqr.c = [1., 0.]
-    ret.lateralTuning.lqr.k = [-110., 451.]
-    ret.lateralTuning.lqr.l = [0.33, 0.318]
+    ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
+    ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
 
-    ret.steerRatio = 14.4
-    ret.steerActuatorDelay = 0.25
-    ret.steerLimitTimer = 2.5
+    ret.steerRatio = 13.8
+    ret.steerActuatorDelay = 0.2
+    ret.steerLimitTimer = 2.0
 
     ret.steerRateCost = 0.55
 
     ret.steerMaxBP = [0.]
-    ret.steerMaxV = [1.3]
+    ret.steerMaxV = [1.7]
 
     ###################################################
     # scc smoother
