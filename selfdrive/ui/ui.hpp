064f55d9530460ef2790311f92c7b73ac6f0eb6a
boxkon <ghkdxo14@gmail.com>
2020-12-03 21:05:47 +0900

    브레이크관련 수정

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -137,7 +137,7 @@ typedef struct UIScene {
   std::string alert_text2;
   std::string alert_type;
   cereal::ControlsState::AlertSize alert_size;
-  bool  brakePress;
+  bool brakePress;
   bool brakeLights;
 
   cereal::HealthData::HwType hwType;
