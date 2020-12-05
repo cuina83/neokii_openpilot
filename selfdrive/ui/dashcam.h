49dc23cc1c253df39a21b2f3188ebfbb799b669f
Sanghyun <ghkdxo14@gmail.com>
2020-12-05 21:10:40 +0900

    REC 위치조정

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -286,7 +286,7 @@ static void screen_draw_button(UIState *s, int touch_x, int touch_y) {
       else {
         nvgFillColor(s->vg, nvgRGBA(255, 255, 255, 200));
       }
-      nvgText(s->vg,btn_x-35,btn_y+50,"REC",NULL);
+      nvgText(s->vg,btn_x+btn_w/2, btn_y+btn_h/2,"REC",NULL);
   }
 
   if (captureState == CAPTURE_STATE_CAPTURING) {
