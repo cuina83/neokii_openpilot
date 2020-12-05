a23790b077addad94f34eab27d88a21c3c13f463
Sanghyun <ghkdxo14@gmail.com>
2020-12-05 19:17:51 +0900

    네오키 업데이트

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -8,8 +8,7 @@ from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.hyundai.hyundaican import create_lkas11, create_clu11, create_lfa_mfa, \
   create_scc11, create_scc12, create_scc13, create_scc14, \
   create_mdps12, create_spas11, create_spas12, create_ems11
-from selfdrive.car.hyundai.scc_smoother import SccSmoother, WAIT_COUNT_MIN, WAIT_COUNT_MAX, \
-  ALIVE_COUNT_MIN, ALIVE_COUNT_MAX
+from selfdrive.car.hyundai.scc_smoother import SccSmoother
 from selfdrive.car.hyundai.values import Buttons, SteerLimitParams, CAR, FEATURES
 from opendbc.can.packer import CANPacker
 from selfdrive.config import Conversions as CV
@@ -100,7 +99,6 @@ class CarController():
 
     # gas and brake
     apply_accel = actuators.gas - actuators.brake
-
     apply_accel, self.accel_steady = accel_hysteresis(apply_accel, self.accel_steady)
     apply_accel = clip(apply_accel * ACCEL_SCALE, ACCEL_MIN, ACCEL_MAX)
 
@@ -134,7 +132,7 @@ class CarController():
     # Disable steering while turning blinker on and speed below 60 kph
     if CS.out.leftBlinker or CS.out.rightBlinker:
       self.turning_signal_timer = 0.5 / DT_CTRL  # Disable for 0.5 Seconds after blinker turned off
-    if self.turning_indicator_alert and CS.out.vEgo > 70 * CV.KPH_TO_MS: # 시속70미만에서 상시조향 # set and clear by interface
+    if self.turning_indicator_alert: # set and clear by interface
       lkas_active = 0
     if self.turning_signal_timer > 0:
       self.turning_signal_timer -= 1
@@ -215,9 +213,9 @@ class CarController():
         can_sends.append(create_clu11(self.packer, self.resume_cnt, CS.scc_bus, CS.clu11, Buttons.RES_ACCEL, clu11_speed))
         self.resume_cnt += 1
 
-        if self.resume_cnt >= ALIVE_COUNT_MIN:
+        if self.resume_cnt >= 8:
           self.resume_cnt = 0
-          self.resume_wait_timer = randint(WAIT_COUNT_MIN, WAIT_COUNT_MAX)
+          self.resume_wait_timer = SccSmoother.get_wait_count()
 
     # reset lead distnce after the car starts moving
     elif self.last_lead_distance != 0:
