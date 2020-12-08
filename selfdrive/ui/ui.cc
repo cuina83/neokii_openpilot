6e06a046c89614eb6c5976a3ccd0cfaa01dce9c7
boxkon <ghkdxo14@gmail.com>
2020-12-08 13:16:42 +0900

    차선폭 표시 관련

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -186,6 +186,19 @@ void update_sockets(UIState *s) {
    if (sm.updated("pathPlan"))
    {
     scene.path_plan = sm["pathPlan"].getPathPlan();
+    auto data = sm["pathPlan"].getPathPlan();
+
+    scene.pathPlan.laneWidth = data.getLaneWidth();
+    scene.pathPlan.cProb = data.getCProb();
+    scene.pathPlan.lProb = data.getLProb();
+    scene.pathPlan.rProb = data.getRProb();
+
+    auto l_list = data.getLPoly();
+    auto r_list = data.getRPoly();
+
+    scene.pathPlan.lPoly = l_list[3];
+    scene.pathPlan.rPoly = r_list[3];
+
    }
 
    if (sm.updated("gpsLocationExternal"))
